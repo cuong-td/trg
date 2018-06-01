48d92a15d16b25560f1c68a3a9cc9e7f74da273c
fwthanh <fwthanh@gmail.com>
2018-06-01 21:52:59 +0700

    sha1 + sha256

--- meatworks/ViewController.swift
+++ meatworks/ViewController.swift
@@ -178,5 +178,23 @@ public extension Sequence {
     }
 }
 
-
+extension String {
+    func sha1() -> String {
+        let data = self.data(using: String.Encoding.utf8)!
+        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA1($0, CC_LONG(data.count), &digest)
+        }
+        let hexBytes = digest.map { String(format: "%02hhx", $0) }
+        return hexBytes.joined()
+    }
+    func sha256() -> String {
+        let data: Data = self.data(using: String.Encoding.utf8)! as Data
+        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA256($0, CC_LONG(data.count), &hash)
+        }
+        return Data(bytes: hash).description
+    }
+}
 
--- meatworks/ViewController/LoginViewController.swift
+++ meatworks/ViewController/LoginViewController.swift
@@ -52,7 +52,9 @@ class LoginViewController: UIViewController {
         self.view.endEditing(true)
         SVProgressHUD.setDefaultMaskType(.clear)
         SVProgressHUD.show()
-        MService.shared.loginAccount(username: tfUsername.text!, password: tfPassword.text!) { (userId) in
+        let password: String = "[\(Meatworks.userInfo?.currentUserId)]" + tfPassword.text?.sha1()
+        let sha256Password = password.sha256()
+        MService.shared.loginAccount(username: tfUsername.text!, password: sha256Password!) { (userId) in
             if (userId != nil) {
                 self.viewQrCode.isHidden = false
                 self.imgCode.image = QRCode.generateImage(userId!, avatarImage: nil)
@@ -74,7 +76,7 @@ class LoginViewController: UIViewController {
         appDelegate.window?.rootViewController = initialViewController
         appDelegate.window?.makeKeyAndVisible()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
