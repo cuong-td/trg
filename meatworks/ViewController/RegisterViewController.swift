043f0ab2e2a16fd6d6b80642c0cfa07c9678b8b8
fwthanh <fwthanh@gmail.com>
2018-06-22 12:50:35 +0700

    encode url

--- meatworks/ViewController/RegisterViewController.swift
+++ meatworks/ViewController/RegisterViewController.swift
@@ -74,7 +74,7 @@ class RegisterViewController: UIViewController {
     
     @IBAction func signUpAction(_ sender: AnyObject) {
         
-        if tfEmail.text == "" || (tfPassword.text != "" || tfPassword.text != tfRetypePassword.text) {
+        if tfEmail.text == "" || (tfPassword.text == "" || tfPassword.text != tfRetypePassword.text) {
             let alert = UIAlertController(title: "alertRequiredFields".localized(), message: nil, preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Ok".localized(), style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
